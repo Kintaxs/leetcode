3fe40ef3a8756749c6a6f120f0c43cb6e2541088
kintaxxs <travis2381@gmail.com>
2017-11-03 10:53:12 +0800

    add 350

---
+++ python/350.intersection-of-two-arrays-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+        res = list()
+
+        if len(nums1) > len(nums2):
+            for i in nums2:
+                if i in nums1:
+                    res.append(i)
+                    nums1.remove(i)
+        else:
+            for i in nums1:
+                if i in nums2:
+                    res.append(i)
+                    nums2.remove(i)
+        return res
